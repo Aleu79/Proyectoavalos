40c2cc6f2bc1f360a2bc0b203b459a1750e60812
abigc23 <rocioguticao@gmail.com>
2024-06-27 23:19:21 -0300

    commit estilo

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // App.js
 import React, { useState } from 'react';
-import { View, TextInput, Button, Alert, StyleSheet } from 'react-native';
+import { View,Text, TextInput, Button, Alert, StyleSheet, TouchableOpacity } from 'react-native';
 import { collection, addDoc } from 'firebase/firestore';
 import { firestore } from './firebase'; // Importamos la configuracion de firestore
 
@@ -33,30 +33,58 @@ const App = () => {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.titulo}>Ingresa una palabra</Text>
       <TextInput
         style={styles.input}
         value={text}
         onChangeText={setText}
-        placeholder="Escribe una palabra"
+        placeholder="Palabra"
       />
-      <Button title="Guardar" onPress={handleSave} />
+      <TouchableOpacity
+        onPress={handleSave}
+        style={styles.boton}
+        >
+        <Text style={styles.texto}>Guardar</Text>
+      </TouchableOpacity> 
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
     padding: 20,
   },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingHorizontal: 10,
+  titulo: {
+    fontSize: 24,
+    fontStyle: 'bold',
+    marginBottom: 10,
+    fontWeight: 'bold',
   },
+  input: {
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: '#f1f1f1',
+    padding: 10,
+    paddingStart: 20,
+    borderRadius: 35,
+    marginTop: 20, 
+    width: '80%',
+    backgroundColor: '#ffff',
+  },
+  boton: {
+    marginTop: 20,
+    borderRadius: 35,
+    width: '50%',
+    backgroundColor: '#2596be',
+    alignItems: 'center',
+    padding: 8,
+  },
+  texto:{
+    color: '#f1f1f1',
+    fontSize: 15,
+  }
 });
 
 export default App;
